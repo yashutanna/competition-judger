87ec3b0b80240cf76dab5513fd4c22f4fb79eccb
yashu <yashu.tanna@outlook.com>
2018-08-06 22:00:42 +0200

    moved submissions controller method to team controller root

--- src/main/java/za/co/judge/controllers/QuestionController.java
+++ src/main/java/za/co/judge/controllers/QuestionController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import za.co.judge.domain.Question;
 import za.co.judge.domain.Submission;
 import za.co.judge.domain.Team;
@@ -16,8 +15,6 @@ import za.co.judge.services.TeamService;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.security.Principal;
-import java.sql.Date;
-import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
@@ -59,12 +56,6 @@ public class QuestionController {
         Submission submission = initializeSubmissionForQuestion(questionName, team.get(), 10);
         questionService.getTestSetFileForSubmission(submission, response.getOutputStream());
         response.flushBuffer();
-    }
-
-    @PostMapping(value = "/submit")
-    public Submission submit(@RequestParam("file") MultipartFile file, Principal principal) throws IOException {
-        Long submissionTime = Date.from(Instant.now()).getTime();
-        return submissionService.checkSubmission(file, principal.getName(), submissionTime);
     }
 
     private Submission initializeSubmissionForQuestion(String name, Team team, int setSize) {
--- src/main/java/za/co/judge/controllers/TeamController.java
+++ src/main/java/za/co/judge/controllers/TeamController.java
@@ -4,12 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import za.co.judge.domain.Member;
 import za.co.judge.domain.Submission;
 import za.co.judge.domain.Team;
 import za.co.judge.services.SubmissionService;
 import za.co.judge.services.TeamService;
 
+import java.io.IOException;
+import java.security.Principal;
+import java.sql.Date;
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
@@ -41,6 +46,12 @@ public class TeamController {
     @GetMapping("/{name}/submissions")
     public ResponseEntity<List<Optional<Submission>>> getTeamSubmissions(@PathVariable("name") String name){
         return new ResponseEntity<>(submissionService.getAttemptedSubmissionsForTeam(name), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/{name}/submissions")
+    public Submission submit(@RequestParam("file") MultipartFile file, Principal principal) throws IOException {
+        Long submissionTime = Date.from(Instant.now()).getTime();
+        return submissionService.checkSubmission(file, principal.getName(), submissionTime);
     }
     
 }
