38bd57326a1e6e093ddcfa4bb839880bb3eb491d
yashu <yashu.tanna@outlook.com>
2018-08-17 21:43:20 +0200

    added role check for leaderboard

--- src/main/java/za/co/judge/controllers/LeaderboardController.java
+++ src/main/java/za/co/judge/controllers/LeaderboardController.java
@@ -1,6 +1,7 @@
 package za.co.judge.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class LeaderboardController {
     private LeaderboardService leaderboardService;
 
     @GetMapping("")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public List<TeamStanding> getLeaderBoard(){
         return leaderboardService.getLeaderBoard();
     }
