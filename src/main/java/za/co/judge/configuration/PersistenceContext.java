da564d6d72c23a5a99ee5fa873bf248c7a4c20af
yashu <yashu.tanna@outlook.com>
2018-08-17 20:43:41 +0200

    updated password for neo4j

--- src/main/java/za/co/judge/configuration/PersistenceContext.java
+++ src/main/java/za/co/judge/configuration/PersistenceContext.java
@@ -1,6 +1,5 @@
 package za.co.judge.configuration;
 
-import org.neo4j.ogm.session.LoadStrategy;
 import org.neo4j.ogm.session.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -28,7 +27,7 @@ public class PersistenceContext {
     @Bean
     public org.neo4j.ogm.config.Configuration configuration() {
         return new org.neo4j.ogm.config.Configuration.Builder()
-                .credentials("neo4j", "test")
+                .credentials("neo4j", "neo4j")
                 .uri("bolt://127.0.0.1:7687")
                 .build();
     }
